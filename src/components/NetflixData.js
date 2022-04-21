1fee9a4f443b6602ef296c06f11cce6166376968
unknown <rathorepranjal2@gmail.com>
2022-04-21 19:45:04 +0530

    react project

---
+++ src/components/NetflixData.js
@@ -0,0 +1,52 @@
+const mdata = [
+    {
+        mname : "Kota factory",
+        mimgsrc : "https://1.bp.blogspot.com/-O9x376nMowM/YTscxPsPTzI/AAAAAAAAbRs/TiRmsj9EsgAm-qCZMs6WiVNvHOd3nlRbACLcBGAsYHQ/w320-h180/Kota%2BFactory%2B2%2BTrailer.webp",
+        mheading : "Netflix Original Series",
+        mlink : "https://www.netflix.com/in/title/81249783"
+    },
+    {
+        mname : "Maid",
+        mimgsrc : "https://www.historyvshollywood.com/reelfaces/images/2021/09/maid/social.jpg",
+        mheading : "Netflix Original Series",
+        mlink : "https://www.netflix.com/maid"
+    },  
+    {
+        mname : "Squid Game",
+        mimgsrc : "https://www.koimoi.com/wp-content/new-galleries/2021/11/netflix-yields-ground-on-squid-game-dispute-with-south-korea-001.jpg",
+        mheading : "Netflix Original Series",
+        mlink : "https://www.netflix.com/squidgame"
+    },
+    {
+        mname : "The chair",
+        mimgsrc : "https://resizing.flixster.com/xzm3VNIVmH5EFEc6zCBNOR2n9Gk=/ems.ZW1zLXByZC1hc3NldHMvdHZzZXJpZXMvUlRUVjk2NDY1NC53ZWJw",
+        mheading :" Netflix Original Series",
+        mlink : "https://www.netflix.com/thechair"
+    },
+    {
+        mname : "Hell Bound",
+        mimgsrc : "https://m.media-amazon.com/images/M/MV5BNzU1ODk0ZWUtMzdkYS00YmI2LWI1NjItOTg0NjQxNGY5MmNlXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_FMjpg_UX1000_.jpg",
+        mheading :" Netflix Original Series",
+        mlink : "https://www.netflix.com/hellbound"
+    },
+    {
+        mname : "Sacred Games",
+        mimgsrc : "https://filmdaily.co/wp-content/uploads/2020/06/sacredgames-lede-1-1536x864.jpg",
+        mheading : "Netflix Original Series",
+        mlink : "https://www.netflix.com/sacredgames"
+    },
+    {
+        mname : "Vampires",
+        mimgsrc : "https://m.media-amazon.com/images/M/MV5BZDJhNjA5YTAtMTNhYy00YWYyLWE0OTYtMWQwZmYyMDM5ZGIyXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+        mheading :" Netflix Original Series",
+        mlink : "https://www.netflix.com/vampires"
+    },
+    {
+        mname : "House of secrets",
+        mimgsrc : "https://resizing.flixster.com/z_LTK4-_wJFFQTflAc3twhlqpMc=/206x305/v2/https://flxt.tmsimg.com/assets/p20700185_b_v13_aa.jpg",
+        mheading :" Netflix Original Series",
+        mlink : "https://www.netflix.com/houseofsecrets"
+    }
+];
+
+export default mdata;
